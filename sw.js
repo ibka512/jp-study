fefd61f8669a4b3344d4551dbd1b7f19778e2e4e
ibka512 <moyu54433@gmail.com>
2026-07-22 17:15:10 +0800

    更新移除印章后的缓存版本

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('./wordbanks/assets.js');
 
 const CACHE_PREFIX = 'zhongri-';
-const CACHE_NAME = 'zhongri-shell-v13';
+const CACHE_NAME = 'zhongri-shell-v14';
 const LOCAL_ASSETS = [
   './',
   './index.html',
